570ddcdd5c29bafd750b7cf6c67dc64195874f8c
cherylbai7 <63435689+chrryyy@users.noreply.github.com>
2023-06-07 13:08:22 -0400

    Working on pulling datasets

--- src/Mathematics.js
+++ src/Mathematics.js
@@ -17,6 +17,8 @@ const Mathematics = () => {
   const [dois, setDOIS] = useState([]);
   const [mathObjects, setMathObjects] = useState([]);
 
+  let objects = [];
+
   useEffect(() => {
     setImgUrl("");
     setTitle("");
@@ -30,9 +32,21 @@ const Mathematics = () => {
       for(var i = 0; i < response.data.data.length; i += 1){
         dois.push(response.data.data[i].identifier);
       }
+      console.log(dois);
+
+      dois.forEach(doi => {
+        axios.get("https://dataverse.lib.virginia.edu/api/datasets/:persistentId/?persistentId=doi:10.18130/"+ doi)
+        .then(object => {
+          objects.push(object.data);
+        })
+        .catch((error) => console.log("Error: ", error));
+      })
+
+      console.log(objects);
     })
     .catch((error) => console.log("Error: ", error))
 
+    console.log("Finished fetching");
   }, [])
 
   return (
