b3e478b660a974e6a94c9a0910ab5a2ca90059ff
cherylbai7 <63435689+chrryyy@users.noreply.github.com>
2023-06-08 19:45:02 -0400

    React collapsed package installed

--- src/Components/ObjectCard.js
+++ src/Components/ObjectCard.js
@@ -1,10 +1,12 @@
 import React from "react";
+import useCollapse from 'react-collapsed';
 import "../Styles/ObjectCard.css";
+
 
 const ObjectCard = (props) => {
 
     return (
-        <div class="card">
+        <div class="card" >
             <img id="img" src={props.objImageUrl} alt="Object Thumbnail Not Found"></img>
             <div class="container">
                 <h3><b id="object">{props.objTitle}</b></h3>
