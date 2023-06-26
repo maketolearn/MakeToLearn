33cd0536ed7dc694070e299237c097730dd19bdb
cherylbai7 <63435689+chrryyy@users.noreply.github.com>
2023-06-25 20:21:04 -0400

    Added citation format

--- src/Components/Object.js
+++ src/Components/Object.js
@@ -14,8 +14,15 @@ const Object = () => {
     const [title, setTitle] = useState("");
     const [desc, setDesc] = useState("");
     const [introSentence, setIntroSentence] = useState("");
+    const [developerName, setDeveloperName] = useState("");
+    const [developerLink, setDeveloperLink] = useState("");
     const [instructionalResourcesUrl, setInstructionalResourcesUrl] = useState(""); //download url for the instructional resources zip file
     const [fabricationGuideUrl, setFabricationGuideUrl] = useState(""); //download url for the fabrication guide zip file
+
+    //citation fields
+    const [authorsFormmated, setAuthorsFormatted] = useState("");
+    const [year, setYear] = useState("");
+    const [depositDate, setDepositDate] = useState("");
 
     let doiPieces = [];
     doiPieces.push(doi.substring(0, 2));
@@ -25,7 +32,10 @@ const Object = () => {
     useEffect(() => {
         axios.get("https://dataverse.lib.virginia.edu/api/datasets/:persistentId/?persistentId=doi:10.18130/"+ dataverseDoi)
         .then(object => {
+            console.log(object.data.data.latestVersion);
             setTitle(object.data.data.latestVersion.metadataBlocks.citation.fields[0].value);
+            let author = object.data.data.latestVersion.metadataBlocks.citation.fields[1].value[0].authorName.value;
+            formatAuthors(author);
             let description = object.data.data.latestVersion.metadataBlocks.citation.fields[3].value[0].dsDescriptionValue.value;
 
             setIntroSentence(description.substring(0, description.indexOf(".")) + ".");
@@ -51,10 +61,57 @@ const Object = () => {
             setImgUrl("https://dataverse.lib.virginia.edu/api/access/datafile/" + imgID);
             setInstructionalResourcesUrl("https://dataverse.lib.virginia.edu/api/access/datafile/" + instructionalID);
             setFabricationGuideUrl("https://dataverse.lib.virginia.edu/api/access/datafile/" + fabricationID);
+
+            //setting link to developer
+            let developer = object.data.data.latestVersion.metadataBlocks.citation.fields[7].value;
+            if(developer.includes("https")){
+                setDeveloperName(developer.substring(0, developer.indexOf(",")));
+                setDeveloperLink(developer.substring(developer.indexOf("https")));
+            }
+            
+            let depositDate = object.data.data.latestVersion.metadataBlocks.citation.fields[9].value;
+            setYear(depositDate.substring(0, 4));
+            formatDepositDate(depositDate);
+
             
         })
         .catch((error) => console.log("Error: ", error));
     }, [])
+
+    const formatAuthors = (author) => {
+        //two authors
+        if(author.includes("&")){
+            let authors = [];
+            authors = author.split(" & ");
+            let authorLastName1 = authors[0].substring(0, authors[0].indexOf(","));
+            let authorFirstInitial1 = authors[0].charAt(authorLastName1.length + 2);
+            let authorLastName2 = authors[1].substring(0, authors[1].indexOf(","));
+            let authorFirstInitial2 = authors[1].charAt(authorLastName2.length + 2);
+            setAuthorsFormatted(authorLastName1 + ", " + authorFirstInitial1 + ". & " + authorLastName2 + ", " + authorFirstInitial2 + ".")
+        }
+        else {
+            let authorLastName = author.substring(0, author.indexOf(","));
+            let authorFirstInitial = author.charAt(authorLastName.length + 2);
+            setAuthorsFormatted(authorLastName + ", " + authorFirstInitial + ".");
+        }
+    }
+
+    const formatDepositDate = (depositDate) => {
+        let date = depositDate.split("-");
+        let year = date[0];
+        let month = date[1];
+        if(date[1].includes("0")){
+            month = month[1];
+        }
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        month = months[month - 1]
+        let day = date[2];
+        if(day[0] === "0"){
+            day = day[1];
+        }
+        setDepositDate(month + " " + day + ", " + year);
+
+    }
 
     return (
         <div>
@@ -80,7 +137,7 @@ const Object = () => {
                             <div id="box">
                                 <b>Developer</b>
                                 <br></br>
-                                <p class="detail"><a>Link to developer</a></p>
+                                <p class="detail"><a href={developerLink} target="_blank">{developerName}</a></p>
                                 
                                 <b>Subject</b>
                                 <br></br>
@@ -110,7 +167,7 @@ const Object = () => {
                                 </ul>
                                 
                             <h4> Citation </h4>
-                                <p> Citation goes here </p>
+                                <p>{authorsFormmated} ({year}). <em>{title}</em> [Educational Object]. <em>Educational CAD Model Library</em>. Deposited {depositDate}. NTLS Coalition. doi:10.18130/{dataverseDoi} </p>
                         </div>
                     </div>
                 </div>
