88ae6a6c5c1e2c4be17570819317e0f6d8c5ab27
cherylbai7 <63435689+chrryyy@users.noreply.github.com>
2023-09-14 08:49:56 -0400

    Slight styling changes

--- src/Components/ObjectCard.js
+++ src/Components/ObjectCard.js
@@ -28,7 +28,7 @@ const ObjectCard = (props) => {
                         {doi === "00000C144" ? 
                         <p id="Description">This dataset of fossil horse teeth published on  <a href="https://www.morphosource.org/" target="_blank"> Morphosource </a> has been selected by Florida Museum scientists to help K12 students understand concepts related to horse evolution.</p> 
                         // the .match function finds the next space after the number of characters input 
-                        : <p id="Description">{props.objDescription.match(/^.{150}\w*/)}</p>}
+                        : <p id="Description">{props.objDescription}</p>}
                     </div>
                 </div>
             </Link>
